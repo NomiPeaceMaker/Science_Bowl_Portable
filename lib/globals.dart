4dc19c7a20402d77b7bf3966292d2b402f306d4b
Your Name <nbutt50@hotmail.com>
2020-04-22 00:51:15 +0500

    starting again

---
+++ lib/globals.dart
@@ -0,0 +1,4 @@
+library science_bowl_portable.globals;
+
+bool isLoggedIn = false;
+var name1 = 'nomipeacemaker';
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'screens/edit_account.dart';
+import "package:science_bowl_portable/globals.dart";
 
 void main() => runApp(MyApp());
 
@@ -13,16 +15,6 @@ class MyApp extends StatelessWidget {
           color: Color.fromARGB(200, 255, 255, 255),
         
       ),
-        
-        // This is the theme of your applsication.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         brightness: Brightness.light,
         primaryColor: Color(0xFFF8B400),
         accentColor: Colors.cyan[600],
@@ -56,7 +48,7 @@ enum settings {
   help, report
 }
 
-var _name = 'NomiPeaceMaker';
+var name1 = 'NomiPeaceMaker';
 
 class _MyHomePageState extends State<MyHomePage> {
   settings _selection;
@@ -73,45 +65,39 @@ class _MyHomePageState extends State<MyHomePage> {
                   TextSpan(
                     children: <TextSpan>[
                       TextSpan(text: "Welcome,\n",style: TextStyle(fontWeight: FontWeight.bold, fontSize: 44)),
-                      TextSpan(text: "$_name",style: TextStyle(fontWeight: FontWeight.normal, fontSize: 20)),
+                      TextSpan(text: "$name1",style: TextStyle(fontWeight: FontWeight.normal, fontSize: 20)),
                     ],
                     
                   )
                 ),
               ),
             ),
-            ListTile( leading:Image(
+            ListTile(
+              leading:Image(
               image: AssetImage('a.png'),
               ),
               title: GestureDetector(
-        onTap: (){
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => edit_account()),
-          );
-        },
-        child: Container(
-          child: Text("Edit Account",
-          textAlign: TextAlign.left,
-              overflow: TextOverflow.ellipsis,
-              style: TextStyle(fontWeight: FontWeight.normal, color: Color(0xFFF8B400)),
+                onTap: (){
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => edit_account()),
+                  );
+                },
+                child: Container(
+                  child: Text("Edit Account",
+                  textAlign: TextAlign.left,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(fontWeight: FontWeight.normal, color: Color(0xFFF8B400)),
+                  ),
+                )
               ),
-        )
-    ),
-              // Text(
-              // 'Edit Account',
-              
-              // textAlign: TextAlign.left,
-              // overflow: TextOverflow.ellipsis,
-              // style: TextStyle(fontWeight: FontWeight.normal, color: Color(0xFFF8B400)),
-              // )
             ),
               ListTile(),
               ListTile(),
               ListTile(),
           ],
         )
-        ),
+      ),
         appBar: AppBar(
           title: Align( alignment: Alignment.center,
             child: const Text('SBP'),
@@ -141,7 +127,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 Row(
                   children: [
                     Column(children: [Text(
-                      "Welcome $_name!"
+                      "Welcome $name1!"
                     )]
                     ),
                     Column(children: [Text(
@@ -182,79 +168,27 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class edit_account extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          title: Align( alignment: Alignment.center,
-            child: Text('Account'),
-          ),
-          backgroundColor: Color(0xFFF8B400),
-      ),
-      body: Column(
-        children: [
-          Row(children: <Widget>[
-            Text("Username: "), Text("$_name"),
-            Container(alignment: Alignment.centerRight,
-            child: Text("                           edit",style: TextStyle(color: Colors.red,),)
-            ),
-          ],
-          )
-        ]),
+// class edit_account extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//           title: Align( alignment: Alignment.center,
+//             child: Text('Account'),
+//           ),
+//           backgroundColor: Color(0xFFF8B400),
+//       ),
+//       body: Column(
+//         children: [
+//           Row(children: <Widget>[
+//             Text("Username: "), Text("$_name"),
+//             Container(alignment: Alignment.centerRight,
+//             child: Text("                           edit",style: TextStyle(color: Colors.red,),)
+//             ),
+//           ],
+//           )
+//         ]),
         
-      );
-  }
-}
-  // Widget build(BuildContext context) {
-  //   // This method is rerun every time setState is called, for instance as done
-  //   // by the _incrementCounter method above.
-  //   //
-  //   // The Flutter framework has been optimized to make rerunning build methods
-  //   // fast, so that you can just rebuild anything that needs updating rather
-  //   // than having to individually change instances of widgets.
-  //   return Scaffold(
-  //     // appBar: AppBar(
-  //     //   // Here we take the value from the MyHomePage object that was created by
-  //     //   // the App.build method, and use it to set our appbar title.
-  //     //   title: Text(widget.title),
-  //     // ),
-  //     body: Align(
-  //       alignment: Alignment.center,
-  //       // Center is a layout widget. It takes a single child and positions it
-  //       // in the middle of the parent.
-  //       child: Column(
-  //         // Column is also a layout widget. It takes a list of children and
-  //         // arranges them vertically. By default, it sizes itself to fit its
-  //         // children horizontally, and tries to be as tall as its parent.
-  //         //
-  //         // Invoke "debug painting" (press "p" in the console, choose the
-  //         // "Toggle Debug Paint" action from the Flutter Inspector in Android
-  //         // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-  //         // to see the wireframe for each widget.
-  //         //
-  //         // Column has various properties to control how it sizes itself and
-  //         // how it positions its children. Here we use mainAxisAlignment to
-  //         // center the children vertically; the main axis here is the vertical
-  //         // axis because Columns are vertical (the cross axis would be
-  //         // horizontal).
-  //         mainAxisAlignment: MainAxisAlignment.start,
-  //         children: <Widget>[
-  //           Text(
-  //             'Press here to start the game!',
-  //           ),
-  //           Text(
-  //             '$_counter',
-  //             style: Theme.of(context).textTheme.display1,
-  //           ),
-  //         ],
-  //       ),
-  //     ),
-  //     floatingActionButton: FloatingActionButton(
-  //       onPressed: _incrementCounter,
-  //       tooltip: 'Press to start game!',
-  //       child: Icon(Icons.apps),
-  //     ), // This trailing comma makes auto-formatting nicer for build methods.
-  //   );
-  // }
-
+//       );
+//   }
+// }
