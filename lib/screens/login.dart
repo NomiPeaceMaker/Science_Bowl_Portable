c1456afb1795f6bac194cffad7524a1045823165
lailagit2 <57248768+lailagit2@users.noreply.github.com>
2020-04-26 04:48:27 +0500

    Update login.dart

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -4,6 +4,7 @@ import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:sciencebowlportable/screens/username.dart';
+import 'package:sciencebowlportable/globals.dart';
 
 // void main() => runApp(Login());
 
@@ -28,32 +29,31 @@ class _LoginState extends State<Login> {
     return Scaffold(
       body: Container(
           child: Center(
-            child: loggedIn
-                ? Text("Logged In! :)",
-                style: TextStyle(color: Colors.white, fontSize: 40))
-                : Stack(
-              children: <Widget>[
-                SizedBox.expand(
-                  child: _buildSignUpText(),
-                ),
-                Container(
-                  child: Center(
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      // wrap height
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      // stretch across width of screen
-                      children: <Widget>[
-                        _buildFacebookLoginButton(),
-                        SizedBox(height: 50),
-                        _buildGoogleLoginButton(),
-                      ],
-                    ),
+        child: loggedIn
+            ? Text("Logged in")
+            : Stack(
+                children: <Widget>[
+                  SizedBox.expand(
+                    child: _buildSignUpText(),
                   ),
-                )
-              ],
-            ),
-          )),
+                  Container(
+                    child: Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        // wrap height
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        // stretch across width of screen
+                        children: <Widget>[
+                          _buildFacebookLoginButton(),
+                          SizedBox(height: 50),
+                          _buildGoogleLoginButton(),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+      )),
     );
   }
 
@@ -70,7 +70,7 @@ class _LoginState extends State<Login> {
             icon: FaIcon(FontAwesomeIcons.google),
             color: Color(0xFFEA4335),
             shape:
-            RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
             textColor: Colors.white,
             label: Text("   Connect with Google",
                 style: TextStyle(
@@ -95,7 +95,7 @@ class _LoginState extends State<Login> {
             icon: FaIcon(FontAwesomeIcons.facebookF),
             color: Color(0xff1977F3),
             shape:
-            RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
             textColor: Colors.white,
             label: Text(
               "   Connect with Facebook",
@@ -126,17 +126,18 @@ class _LoginState extends State<Login> {
     _handleSignIn(type).then((result) {
       if (result == 1) {
         setState(() {
-          loggedIn = true;  
+          loggedIn = true;
         });
         Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Username()),
-                  );
-      } else { //CHANGE THIS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+          context,
+          MaterialPageRoute(builder: (context) => Username()),
+        );
+      } else {
+        //CHANGE THIS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Username()),
-                  );
+          context,
+          MaterialPageRoute(builder: (context) => Username()),
+        );
       }
       // firstusername();
     });
@@ -149,9 +150,10 @@ class _LoginState extends State<Login> {
         final accessToken = facebookLoginResult.accessToken.token;
         if (facebookLoginResult.status == FacebookLoginStatus.loggedIn) {
           final facebookAuthCred =
-          FacebookAuthProvider.getCredential(accessToken: accessToken);
-          final FirebaseUser user =
+              FacebookAuthProvider.getCredential(accessToken: accessToken);
+          final FirebaseUser Fire_user =
               (await firebaseAuth.signInWithCredential(facebookAuthCred)).user;
+          user.email = Fire_user.email;
           return 1;
         } else
           return 0;
@@ -162,9 +164,10 @@ class _LoginState extends State<Login> {
           final googleAuth = await googleSignInAccount.authentication;
           final googleAuthCred = GoogleAuthProvider.getCredential(
               idToken: googleAuth.idToken, accessToken: googleAuth.accessToken);
-          final FirebaseUser user =
+          final FirebaseUser Fire_user =
               (await firebaseAuth.signInWithCredential(googleAuthCred)).user;
-//          debugPrint(user.getIdTokenResult);
+          user.email = Fire_user.email;
+          print(user.email);
           return 1;
         } catch (error) {
           return 0;
@@ -176,7 +179,7 @@ class _LoginState extends State<Login> {
   Future<FacebookLoginResult> _handleFBSignIn() async {
     FacebookLogin facebookLogin = FacebookLogin();
     FacebookLoginResult facebookLoginResult =
-    await facebookLogin.logInWithReadPermissions(['email']);
+        await facebookLogin.logInWithReadPermissions(['email']);
     switch (facebookLoginResult.status) {
       case FacebookLoginStatus.cancelledByUser:
         print("Cancelled");
