0f896cbac5e635873d56d3475113da195ef292cc
Anushehzm <40184015+Anushehzm@users.noreply.github.com>
2020-04-26 04:28:44 -0500

    Added functionality to Game screen

--- lib/screens/join.dart
+++ lib/screens/join.dart
@@ -4,11 +4,10 @@
 import 'package:flutter/material.dart';
 import 'dart:async'; //for timer
 import "dart:math";
-import 'package:sciencebowlportable/screens/result.dart';
-import 'package:sciencebowlportable/screens/home.dart';
-import 'package:sciencebowlportable/globals.dart';
+import 'package:sciencebowlportable/models/Moderator.dart';
+import 'package:sciencebowlportable/models/Player.dart';
 
- //for random
+import 'package:sciencebowlportable/screens/home.dart'; //for random
 void join() => Pin();
 //Might need to tweak the colour scheme a bit + Red Team or Team A?
 class _WaitingRoom extends StatefulWidget {
@@ -294,20 +293,11 @@ class Pin extends StatefulWidget {
 }
 
 class _PinState extends State<Pin> {
-  // String pin;
-  final myController = TextEditingController();
-
-  @override
-  void dispose() {
-    // Clean up the controller when the widget is disposed.
-    myController.dispose();
-    super.dispose();
-  }
-  
+  String pin;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+
       backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Color(0xffF8B400),
@@ -322,7 +312,6 @@ class _PinState extends State<Pin> {
               children: <Widget>[
                 new Expanded(
                     child: new TextField(
-                      controller: myController,
                       autofocus: true,
                       decoration: new InputDecoration(
                           labelText: 'Match PIN',
@@ -344,7 +333,6 @@ class _PinState extends State<Pin> {
               FlatButton(
                 child: Text('Confirm'),
                 onPressed: () {
-                  pin = myController.text;
                   Navigator.push(
                     context,
                     MaterialPageRoute(builder: (context) => _WaitingRoom()),
@@ -366,10 +354,12 @@ class Game extends StatefulWidget {
 }
 
 class _GameState extends State<Game> {
-  double timeLeft = 3.444;
+//  double timeLeft = 300; //5 mins
+  int _minutes=5;
+  int _seconds=0;
   Color txtClr = Colors.white;
   int redScore = 0;
-  String gamePin= pin;
+  String gamePin="NP5629";
   int greenScore = 0;
   Color bzrBorder=Colors.white;
   Color buzzerClr=Color(0xFFf84b4b);
@@ -377,11 +367,18 @@ class _GameState extends State<Game> {
   String roundName="Toss-up";
   String playerName = "Red Captain";
   bool flip=false;
+  String buf="0";
   bool unavailable=false;
   bool team=true; //true for red, false for green
   int _counter = 5;
-  Timer _timer;
+  Timer _buzzTimer;
+  Timer _gameTimer;
   var element="";
+
+  _GameState(){
+  _startBuzzTimer();
+  _startGameTimer();
+  }
   void evaluate(){
     //random evaluation rn
     setState(() {
@@ -427,17 +424,17 @@ class _GameState extends State<Game> {
     });
   }
 
-  void _startTimer() {
+  void _startBuzzTimer() {
     _counter = 5;
-    if (_timer != null) {
-      _timer.cancel();
+    if (_buzzTimer != null) {
+      _buzzTimer.cancel();
     }
-    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+    _buzzTimer = Timer.periodic(Duration(seconds: 1), (timer) {
       setState(() {
         if (_counter > 0) {
           _counter--;
         } else {
-          _timer.cancel();
+          _buzzTimer.cancel();
           setState(() {
             if (flip){
             unavailable=true;
@@ -446,6 +443,35 @@ class _GameState extends State<Game> {
             buzzerClr=Colors.white;
             bzrBorder=Color(0xffAEAEAE);
           }});
+        }
+      });
+    });
+  }
+  void _startGameTimer() {
+    if (_gameTimer != null) {
+      _gameTimer.cancel();
+    }
+    _gameTimer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        if (_minutes<0)
+          {
+            _gameTimer.cancel();
+          }
+        if (_seconds > 0) {
+          _seconds--;
+          if(_seconds<10)
+            {
+              buf="0";
+            }
+          else
+            {
+              buf="";
+            }
+        }
+        else {
+          buf="";
+          _seconds=59;
+          _minutes-=1;
         }
       });
     });
@@ -464,29 +490,24 @@ class _GameState extends State<Game> {
               color: Color(0xffF8B400),
               child: Center(
                 child: Text("Settings",
-                  style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 25),
+//                  textAlign: TextAlign.center,
+                  style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 18),
                 ),
               ),
             ),
             ListTile(
               leading: Icon(Icons.offline_pin, color: Color(0xffF8B400)),
               title: Text("Game PIN:\n $gamePin",
+//              textAlign: TextAlign.center,
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16,),
               ),
             ),
             ListTile(
-              title: GestureDetector(
-                child: Text("Exit Game",
+              leading: Icon(Icons.exit_to_app, color: Color(0xffF8B400),),
+              title: Text("Exit Game",
+//              textAlign: TextAlign.center,
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16,),
               ),
-                onTap: (){
-                  Navigator.popUntil(context, ModalRoute.withName('/home'));
-                }
-              ),
-
-                
-              leading: Icon(Icons.exit_to_app, color: Color(0xffF8B400),),
-              
             ),
           ],
         ),
@@ -512,7 +533,7 @@ class _GameState extends State<Game> {
                   child: Text(
                     "Red\n"+redScore.toString(),
                     textAlign: TextAlign.center,
-                    style: TextStyle(fontWeight: FontWeight.bold, color: Colors.red, fontSize: 25),
+                    style: TextStyle(fontWeight: FontWeight.bold, color: Colors.red, fontSize: 18),
                   )
               ),
               Card(
@@ -524,18 +545,18 @@ class _GameState extends State<Game> {
                   child: Padding(
                       padding: EdgeInsets.symmetric(horizontal: 35.0, vertical: 10.0),
                       child: Text(
-                        "Time Left\n"+timeLeft.toStringAsFixed(2),
+                        "Time Left\n"+_minutes.toString()+":"+buf+_seconds.toString(),
                         textAlign: TextAlign.center,
-                        style: TextStyle(fontWeight: FontWeight.bold, color:Colors.purple , fontSize: 25),
+                        style: TextStyle(fontWeight: FontWeight.bold, color:Colors.purple , fontSize: 18),
                       )
                   )
               ),
               Padding(
-                  padding: EdgeInsets.all(13),
+                  padding: EdgeInsets.all(15),
                   child: Text(
                     "Green\n"+greenScore.toString(),
                     textAlign: TextAlign.center,
-                    style: TextStyle(fontWeight: FontWeight.bold, color: Colors.lightGreen, fontSize: 25),
+                    style: TextStyle(fontWeight: FontWeight.bold, color: Colors.lightGreen, fontSize: 18),
                   )
               )
             ],
@@ -615,23 +636,41 @@ class _GameState extends State<Game> {
                             {
                               Scaffold.of(context).showSnackBar(
                                 SnackBar(
-                                  content: Icon(Icons.thumb_up),
+                                  content: Icon(Icons.done),
                                   backgroundColor: Colors.lightGreen,
 //                                  animation: ,
                                   duration: Duration(seconds: 2),
                                 ),
                               );
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) => Result()),
-                              );
                             }
+                          else if (element=="Incorrect")
+                          {
+                            Scaffold.of(context).showSnackBar(
+                              SnackBar(
+                                content: Icon(Icons.not_interested),
+                                backgroundColor: Colors.red,
+//                                  animation: ,
+                                duration: Duration(seconds: 2),
+                              ),
+                            );
+                          }
+                          else //penalty
+                          {
+                            Scaffold.of(context).showSnackBar(
+                              SnackBar(
+                                content: Icon(Icons.pan_tool),
+                                backgroundColor: Colors.grey[900],
+//                                  animation: ,
+                                duration: Duration(seconds: 2),
+                              ),
+                            );
+                          }
                         }
                         else
                           {
                             buzzerTxt="Buzz In!";
                             buzzerClr=Color(0xFFf84b4b);
-                            _startTimer();
+                            _startBuzzTimer();
                           }
                         flip=!flip;
                       }
