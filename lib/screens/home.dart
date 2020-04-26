606d589bd1059d442426fcaf06df3da63e74925f
Hajira <hajirazam@gmail.com>
2020-04-26 09:13:51 -0700

    fixed an import

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sciencebowlportable/screens/edit_account.dart';
-import 'package:sciencebowlportable/screens/join.dart';
+import 'package:sciencebowlportable/screens/pin_screen.dart';
 import 'package:sciencebowlportable/screens/moderator.dart';
 import 'package:sciencebowlportable/screens/match_settings.dart';
 import 'package:sciencebowlportable/globals.dart';
