b58404bee6690b5c8dfe94f9683dbdb2455a195d
Anushehzm <40184015+Anushehzm@users.noreply.github.com>
2020-04-27 07:54:17 +0500

    Added questionSet attribute after parsing json files

--- lib/models/Moderator.dart
+++ lib/models/Moderator.dart
@@ -1,18 +1,27 @@
 import 'package:sciencebowlportable/models/User.dart';
 import 'package:sciencebowlportable/models/Team.dart';
-
+import 'package:sciencebowlportable/models/Questions.dart';
 import 'package:sciencebowlportable/globals.dart';
 
 class Moderator extends User {
   String gameDifficulty, gamePin;
   int numberOfQuestion, tossUpTime, bonusTime, gameTime;
   var subjects;
+  //change directory to wherever json files are:
+  Future<List<Question>> questionSet=parser("C:/Users/Zohair/Desktop/Anusheh's Documents/Software Engineering/Development/Science_Bowl_Portable/jsonQuestions/");
   Team redTeam, greenTeam;
 }
 
+//void main()
+//{
+//  Moderator mod=new Moderator();
+//  mod.questionSet.then((List<Question> qs)
+//  {
+//    print("in callback");
+//    for (int i=0; i<25; i++)
+//    {
+//      print(qs[i].subjectType);
+//    }
 //
-//void main() {
-//  Moderator m = Moderator("userName", "user.email");
-//  print(m.gameDifficulty);
-//  m.bonusTime = 8.88;
+//  });
 //}
