f6355df04d5de32c69aead93c530f9458be80484
lailagit2 <lailadodhy@gmail.com>
2020-05-04 05:29:26 +0500

    validate settings

--- lib/screens/match_settings.dart
+++ lib/screens/match_settings.dart
@@ -20,6 +20,7 @@ class MatchSettings extends StatefulWidget {
 //List<bool> isSelectedDifficulty = [false, true];
 List<bool> isSelectedSubject_1 = List.generate(2, (_) => false);
 List<bool> isSelectedSubject_2 = List.generate(3, (_) => false);
+bool subject_selected = false;
 
 class _MatchSettingState extends State<MatchSettings> {
   Server server;
@@ -58,7 +59,7 @@ class _MatchSettingState extends State<MatchSettings> {
     moderator.gameDifficulty = "HighSchool";
     moderator.gameTime = 20;
     moderator.numberOfQuestion = 25;
-    moderator.subjects=[];
+    moderator.subjects = [];
     moderator.subjectsdict = {
       0: false,
       1: false,
@@ -72,6 +73,7 @@ class _MatchSettingState extends State<MatchSettings> {
     moderator.difficultyLevel = 1;
     isSelectedSubject_1 = List.generate(2, (_) => false);
     isSelectedSubject_2 = List.generate(3, (_) => false);
+    subject_selected = false;
   }
 
   onData(Uint8List data) {
@@ -167,9 +169,30 @@ class _MatchSettingState extends State<MatchSettings> {
                         height: 40,
                         child: TextField(
                           onChanged: (text) {
+                            if (text != "") {
+                              var value = int.tryParse(text);
+                              if (value == null) {
+                                moderator.gameTime = 20;
+                                setState(() {});
+                                _notIntDialog();
+                              }
+                            }
                             moderator.gameTime = int.parse(text);
+                            print(moderator.gameTime);
+
+                            print(moderator.gameTime);
+                            if (moderator.gameTime < 1) {
+                              moderator.gameTime = 20;
+                              setState(() {});
+                              _lessThanOneDialog();
+                            } else if (moderator.gameTime > 30) {
+                              moderator.gameTime = 20;
+                              setState(() {});
+                              _gameTimeLimit();
+                            }
                           },
-                          controller: TextEditingController()..text = '20',
+                          controller: TextEditingController()
+                            ..text = moderator.gameTime.toString(),
 //                      initialValue: "20",
                           decoration: InputDecoration(
                               filled: true,
@@ -205,9 +228,26 @@ class _MatchSettingState extends State<MatchSettings> {
                         height: 40,
                         child: TextField(
                           onChanged: (text) {
+                          if (text != "") {
+                          var value = int.tryParse(text);
+                          if (value == null) {
+                          moderator.numberOfQuestion = 25;
+                          setState(() {});
+                          _notIntDialog();
+                          }}
                             moderator.numberOfQuestion = int.parse(text);
+                            if (moderator.numberOfQuestion < 1) {
+                              moderator.numberOfQuestion = 25;
+                              setState(() {});
+                              _lessThanOneDialog();
+                            } else if (moderator.numberOfQuestion > 40) {
+                              moderator.numberOfQuestion = 25;
+                              setState(() {});
+                              _questionLimitDialog();
+                            }
                           },
-                          controller: TextEditingController()..text = '25',
+                          controller: TextEditingController()
+                            ..text = moderator.numberOfQuestion.toString(),
                           decoration: InputDecoration(
                               filled: true,
                               fillColor: Colors.white,
@@ -299,8 +339,10 @@ class _MatchSettingState extends State<MatchSettings> {
                         ],
                         onPressed: (int index) {
                           setState(() {
-                            isSelectedSubject_1[index] = !isSelectedSubject_1[index];
-                            moderator.subjectsdict[index] = !moderator.subjectsdict[index];
+                            isSelectedSubject_1[index] =
+                                !isSelectedSubject_1[index];
+                            moderator.subjectsdict[index] =
+                                !moderator.subjectsdict[index];
                             print(index);
                           });
                         },
@@ -313,7 +355,6 @@ class _MatchSettingState extends State<MatchSettings> {
                         borderRadius: BorderRadius.circular(10.0),
                         //                        constraints: BoxConstraints(minWidth: 65, minHeight: 40),
                         children: <Widget>[
-
                           Padding(
                             padding: EdgeInsets.symmetric(horizontal: 15),
                             child: Text("Biology"),
@@ -376,9 +417,26 @@ class _MatchSettingState extends State<MatchSettings> {
                         height: 40,
                         child: TextField(
                           onChanged: (text) {
+                            if (text != "") {
+                            var value = int.tryParse(text);
+                            if (value == null) {
+                            moderator.tossUpTime = 5;
+                            setState(() {});
+                            _notIntDialog();
+                            }}
                             moderator.tossUpTime = int.parse(text);
+                            if (moderator.tossUpTime < 1) {
+                              moderator.tossUpTime = 5;
+                              setState(() {});
+                              _lessThanOneDialog();
+                            } else if (moderator.tossUpTime > 59) {
+                              moderator.tossUpTime = 5;
+                              setState(() {});
+                              _secondsLimit();
+                            }
                           },
-                          controller: TextEditingController()..text = '05',
+                          controller: TextEditingController()
+                            ..text = moderator.tossUpTime.toString(),
                           decoration: InputDecoration(
                               filled: true,
                               fillColor: Colors.white,
@@ -413,9 +471,26 @@ class _MatchSettingState extends State<MatchSettings> {
                         height: 40,
                         child: TextField(
                           onChanged: (text) {
+                            if (text != "") {
+                              var value = int.tryParse(text);
+                              if (value == null) {
+                                moderator.bonusTime = 20;
+                                setState(() {});
+                                _notIntDialog();
+                              }}
                             moderator.bonusTime = int.parse(text);
+                               if (moderator.bonusTime < 1) {
+                              moderator.bonusTime = 20;
+                              setState(() {});
+                              _lessThanOneDialog();
+                            } else if (moderator.bonusTime > 59) {
+                              moderator.bonusTime = 20;
+                              setState(() {});
+                              _secondsLimit();
+                            }
                           },
-                          controller: TextEditingController()..text = '20',
+                          controller: TextEditingController()
+                            ..text = moderator.bonusTime.toString(),
                           decoration: InputDecoration(
                               filled: true,
                               fillColor: Colors.white,
@@ -475,12 +550,22 @@ class _MatchSettingState extends State<MatchSettings> {
                         });
 
                         await server.start();
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) =>
-                                  Loading(this.server, this.moderator)),
-                        );
+                        subjectValidation();
+                        print(moderator.gameTime);
+                        print(moderator.numberOfQuestion);
+                        print(moderator.tossUpTime);
+                        print(moderator.bonusTime);
+//                        bool validInputs = validations();
+                        if (subject_selected == true) {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    Loading(this.server, this.moderator)),
+                          );
+                        }
+                        else
+                          _chooseSubjectDialog();
                         setState(() {});
                       },
                     )
@@ -492,30 +577,148 @@ class _MatchSettingState extends State<MatchSettings> {
         ));
   }
 
-// _exitDialog() {
-//   showDialog(
-//       context: context,
-//       barrierDismissible: true,
-//       builder: (context) {
-//         return AlertDialog(
-//           title: Text("Exit to Home Page"),
-//           content: Text("Are you sure you want to exit to the home page?"),
-//           actions: <Widget>[
-//             FlatButton(
-//               child: Text("No", style: staystyle),
-//               onPressed: () {
-//                 Navigator.of(context).pop();
-//               },
-//             ),
-//             FlatButton(
-//               child: Text("Exit", style: exitstyle),
-//               onPressed: () {  Navigator.pushAndRemoveUntil(context,
-//                 MaterialPageRoute(builder: (BuildContext context) => MyHomePage(),
-//                 ),
-//                 ModalRoute.withName('/'));},
-//             ),
-//           ],
-//         );
-//       });
-// }
+  void subjectValidation() {
+    moderator.subjectsdict.forEach((key, value) {
+      if (value == true) {
+        subject_selected = true;
+        return;
+      }
+    });
+  }
+
+//  bool validations() {
+//    if (moderator.bonusTime <= 0 ||
+//        moderator.gameTime <= 0 ||
+//        moderator.tossUpTime <= 0 ||
+//        moderator.numberOfQuestion <= 0) {
+//      _lessThanOneDialog();
+//      return false;
+//    } else if (subject_selected == false) {
+//      _chooseSubjectDialog();
+//      return false;
+//    }
+//
+//    return true;
+//  }
+
+  _chooseSubjectDialog() {
+    showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("Subject not selected"),
+            content: Text(
+                "You have to pick minimum one subject in order to proceed"),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("Okay"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        });
+  }
+
+  _lessThanOneDialog() {
+    showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("Invalid Input"),
+            content: Text("Input can not be less than 1"),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("Okay"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        });
+  }
+
+  _gameTimeLimit() {
+    showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("Time Limit"),
+            content: Text("Match can not be longer than 30 minutes"),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("Okay"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        });
+  }
+
+  _questionLimitDialog() {
+    showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("Question Limit"),
+            content: Text("Can not choose more than 40 questions"),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("Okay"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        });
+  }
+
+  _notIntDialog() {
+    showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("Invalid Input"),
+            content: Text("Enter an integer"),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("Okay"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        });
+  }
+
+  _secondsLimit() {
+    showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("Time Limit"),
+            content: Text("Can not be a minute long"),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("Okay"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        });
+  }
 }
